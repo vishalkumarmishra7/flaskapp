316c7772b486f5732b4a91e27e757edee3acbf2e
vishal <vishalkumartcd@gmail.com>
2024-06-11 13:25:15 +0100

    adding csv const

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 
 import csv
-from config import RESOURCE_BENEFITS, RESOURCE_SMOOTHIES
+from config import RESOURCE_SMOOTHIES
 from models import Smoothies, db
 
 
 def initialize_db():
     delete_all_smoothies()
     # delete_all_benefits()
-    insert_all_smoothies("resources/data_source.csv")
+    insert_all_smoothies(RESOURCE_SMOOTHIES)
     # insert_all_benefits(RESOURCE_BENEFITS)
     
 def delete_all_smoothies():
