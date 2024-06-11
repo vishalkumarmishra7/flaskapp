1311149beec9497b0afa420c3678fed711170d4c
vishal <vishalkumartcd@gmail.com>
2024-06-11 12:24:34 +0100

    update db uri

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'replace with generated key here')
 
 # SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
 
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@localhost/exp1"
-
+# SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@localhost/exp1"
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', "sqlite:///project.db")
 
 RESOURCE_SMOOTHIES = "resources/smoothies.csv"
 RESOURCE_BENEFITS = "resources/benefits.csv"
